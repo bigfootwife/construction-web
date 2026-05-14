de62dc3df7b935b34c3e36c69a5fff83ffde2e2c
bospit <davidgumaraol@gmail.com>
2026-05-14 09:59:52 +0800

    Updates

--- backend/app/db.py
+++
@@ -1,6 +0,0 @@
-"""MongoDB Motor client (single instance)."""
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient(os.environ["MONGO_URL"])
-db = client[os.environ["DB_NAME"]]
